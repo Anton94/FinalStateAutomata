c280c16c39af534d6e4adbe5b75e56b39473432e
Anton94_home <antonvdudov@gmail.com>
2018-06-11 23:13:46 +0300

    Add the processing of the command line arguments
    and run the input validation tests

--- TestingApp/TestingApp/Source.cpp
+++ TestingApp/TestingApp/Source.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "FinalStateTransducer.h"
+#include "InputValidator.h"
+#include "Tests.h"
 
+const char * regExpr = "abc:5 * cdf:4 | ";
 
-int main()
+void ProcessCommandLineArguments(int argc, char *argv[])
 {
-	std::cout << "Hello from testing app.\n";
+	for (auto i = 1; i < argc; ++i)
+	{
+		if (std::strcmp(argv[i], "-validateInput") == 0)
+		{
+			std::cout << "---Validating the input string...\n";
+			auto isValid = checkInputCorrectness(regExpr);
+			std::cout << (isValid ? "---[valid]" : "***[NOT valid]")
+				<< ".\n";
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	ProcessCommandLineArguments(argc, argv);
+	RunInputValidationTests();
+
 	return 0;
 }
