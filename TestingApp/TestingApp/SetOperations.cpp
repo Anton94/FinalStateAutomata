2feb565c7cc39315fd611945116704814ab24ffe
Anton94_home <antonvdudov@gmail.com>
2018-06-23 23:21:22 +0300

    Add epsilon closing of a set which contains transitions with outputs
    which can also detect infinite cycles with non-zero outputs

--- TestingApp/TestingApp/SetOperations.cpp
+++ TestingApp/TestingApp/SetOperations.cpp
@@ -6,31 +6,28 @@ void TransitiveClosure(SetOfTransitions& r)
 {
 	/*
 		I will devide them into blocks of processing transitions.
-		First devided by the first transition state('a'); second on the level of "connection"
+		First devided by the first transition state('a'); second on the level of "connection" (number of states to go through to get to the 'one')
 	*/
-	auto cR = r; // {<a, [b]>}, an map of states 'a' and their destinations, i.e. a set of b's ([b])
+	auto cR = r; // {<a, [b]>}, a map of states 'a' and their destinations, i.e. a set of b's ([b])
 
 	for (auto& destinations : cR) // @destinations is a pair <a, [b]> for which the array [b] is all 'b', such that (a, b) belongs to 'cR'
 	{
-		std::unordered_set<size_t> currentlyProccessingDestinations = destinations.second;
+		std::unordered_set<size_t> currentlyProcessingDestinations = destinations.second;
 
-		while (currentlyProccessingDestinations.size() > 0)
+		while (currentlyProcessingDestinations.size() > 0)
 		{
 			std::unordered_set<size_t> newDestinations;
-			for (auto b : currentlyProccessingDestinations)
+			for (auto b : currentlyProcessingDestinations)
 			{
-				const auto it = r.find(b); // it is a pair <b, [c]> for which the array [c] is all 'c', such that (b, c) belongs to 'r'
+				const auto it = r.find(b); // @it is a pair <b, [c]> for which the array [c] is all 'c', such that (b, c) belongs to 'r'
 				if (it != r.end())
 				{
-					bool hasNewDestinationNotInAlreadyProcessed = false;
-
 					// Add only the new once, skip the already added!
 					for (const auto& destination : it->second)
 					{
 						if (destinations.second.find(destination) == destinations.second.end())
 						{
 							newDestinations.insert(destination);
-							hasNewDestinationNotInAlreadyProcessed = true;
 						}
 					}
 				}
@@ -38,14 +35,65 @@ void TransitiveClosure(SetOfTransitions& r)
 
 			// Add the currently processed elements to the destinations of 'a'
 			destinations.second.insert(
-				std::make_move_iterator(currentlyProccessingDestinations.begin()),
-				std::make_move_iterator(currentlyProccessingDestinations.end()));
+				std::make_move_iterator(currentlyProcessingDestinations.begin()),
+				std::make_move_iterator(currentlyProcessingDestinations.end()));
 			// Process the newly added elements (so if they are connected to other states they will be added to the destinations of 'a'
-			currentlyProccessingDestinations = std::move(newDestinations);
+			currentlyProcessingDestinations = std::move(newDestinations);
 		}
 	}
 
 	r = std::move(cR);
+}
+
+void ClosureEpsilon(SetOfTransitionsWithOutputs& r, bool& infinite)
+{
+	/*
+	I will devide them into blocks of processing transitions.
+	First devided by the first transition state('a'); second on the level of "connection" (number of states to go through to get to the 'one')
+	*/
+	auto cR = r; // { (a, [<b, o>]) }, a map of states 'a' and their destinations, i.e. a set of b's and coresponding outputs 'o' ([<b, o>])
+
+	for (auto& destinations : cR) // @destinations is a pair <a, [<b, o>]> for which the array [<b, o>] is all 'b' and its output 'o', such that (a, <b, o>) belongs to 'cR'
+	{
+		std::unordered_set<Transition> currentlyProcessingDestinations = destinations.second;
+
+		while (currentlyProcessingDestinations.size() > 0)
+		{
+			std::unordered_set<Transition> newDestinations;
+			const auto& a = destinations.first;
+			for (auto bAndO : currentlyProcessingDestinations)
+			{
+				const auto it = r.find(bAndO.state); // @it is a pair <b, [<c, o'>]> for which the array [<c, o'>] is all 'c' and its output 'o'', such that (b, <c, o'>) belongs to 'r'
+				if (it != r.end())
+				{
+					// Add only the new once, skip the already added!
+					for (const auto& destination : it->second)
+					{
+						if (destination.state == a && destination.output > 0)
+						{
+							infinite = true;
+							return;
+						}
+
+						if (destinations.second.find(destination) == destinations.second.end())
+						{
+							newDestinations.insert(Transition { destination.state, destination.output + bAndO.output});
+						}
+					}
+				}
+			}
+
+			// Add the currently processed elements to the destinations of 'a'
+			destinations.second.insert(
+				std::make_move_iterator(currentlyProcessingDestinations.begin()),
+				std::make_move_iterator(currentlyProcessingDestinations.end()));
+			// Process the newly added elements (so if they are connected to other states they will be added to the destinations of 'a'
+			currentlyProcessingDestinations = std::move(newDestinations);
+		}
+	}
+
+	r = std::move(cR);
+	infinite = false;
 }
 
 void AddIdentity(SetOfTransitions& r)
--- TestingApp/TestingApp/SetOperations.h
+++ TestingApp/TestingApp/SetOperations.h
@@ -7,12 +7,24 @@ struct Transition
 {
 	size_t state; // The destination state(index in a vector)
 	size_t output; // Some number which is the output on the second ?strip? of the transducer.
+
+	bool operator==(const Transition& right)
+	{
+		return state == right.state && output == right.output;
+	}
 };
 
 
 typedef std::unordered_map<size_t, std::unordered_set<size_t>> SetOfTransitions; // { (a, b) | "a is connected to b" } stored by the first component
+typedef std::unordered_map<size_t, std::unordered_set<Transition>> SetOfTransitionsWithOutputs; // { (a, b, out) | "a is connected to b with output out" } stored by the first component
 
 void TransitiveClosure(SetOfTransitions& r);
+
+// Very similar to the transitive closure with accumulating outputs
+// Additionaly infinity checking, i.e.
+// if there is a cycle of type (a, <b, o>) ---> (b, <c, o'>) ---> ... ---> (z, <a, o'">)
+// and the sum of all o, o', ... , o'" is not 0 then it is a non-trivial one, there could be infinite many outputs.
+void ClosureEpsilon(SetOfTransitionsWithOutputs& r, bool& infinite);
 
 void AddIdentity(SetOfTransitions& r);
 
